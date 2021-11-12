f40285810475209be95b6051338821c6def94102
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-11-12 12:16:43 -0300

    correcting rotate checker

--- checker_srcs/apply_instructions.c
+++ checker_srcs/apply_instructions.c
@@ -6,7 +6,7 @@
 /*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 07:45:06 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2021/11/12 08:47:57 by lenzo-pe         ###   ########.fr       */
+/*   Updated: 2021/11/12 12:09:45 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void	apply_instructions(t_node **stack_a, t_node **stack_b,
 		apply_push(stack_a, stack_b, instruction[1], &push);
 	else if (instruction[0] == 's')
 		apply_all(stack_a, stack_b, instruction[1], &swap);
-	else if (instruction[1] != 'r')
+	else if (instruction[2] == '\0')
 		apply_all(stack_a, stack_b, instruction[1], &rotate);
 	else
 		apply_all(stack_a, stack_b, instruction[2], &reverse_rotate);
