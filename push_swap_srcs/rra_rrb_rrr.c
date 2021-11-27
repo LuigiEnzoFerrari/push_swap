b3bf2517c2be22c686d320ee9f29cedfa975bbc0
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-11-27 13:41:12 -0300

    add space before operator for the sake of norminette

--- push_swap_srcs/rra_rrb_rrr.c
+++ push_swap_srcs/rra_rrb_rrr.c
@@ -10,19 +10,19 @@ void	rra(t_node **stack_a)
 {
 	if (reverse_rotate(stack_a))
 		return ;
-	write(1,"rra\n", 4);
+	write(1, "rra\n", 4);
 }
 
 void	rrb(t_node **stack_a)
 {
 	if (reverse_rotate(stack_a))
 		return ;
-	write(1,"rrb\n", 4);
+	write(1, "rrb\n", 4);
 }
 
 void	rrr(t_node **stack_a, t_node **stack_b)
 {
 	reverse_rotate(stack_a);
 	reverse_rotate(stack_b);
-	write(1,"rrr\n", 4);
+	write(1, "rrr\n", 4);
 }
